5e4446a728f599da80ca8a68aea5bd99061f7341
SpecsNStats <specsnstats@gmail.com>
2021-09-25 22:13:54 -0600

    now able to see scores on screen, and clear highscores button does as intended

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,8 @@ scoreIndicatorEL = document.querySelector("#score-indicator");
 scoreEl = document.querySelector("#score");
 initialsSectionEl = document.querySelector("#initials-section");
 submitInitialsEl = document.querySelector("#submit-initials");
-initialsEl = document.querySelector("#initials")
-scoresEl = document.getElementById("scores")
+initialsEl = document.querySelector("#initials");
+scoresEl = document.getElementById("scores");
 
 var secondsLeft = 20;
 var timer;
@@ -23,10 +23,10 @@ var timer;
 var userScore = 0;
 
 // - need a dynamic page that starts with the words "Quiz Challenge" on the screen with some basic words.
-var page = window.location.href
-console.log(page)
+var page = window.location.href;
+console.log(page);
 if (page.includes("index")) {
-    buttonAlwaysEl.addEventListener("click", question1);
+  buttonAlwaysEl.addEventListener("click", question1);
 }
 
 //         -create a timer that starts at 20s, and increments down 1 each second
@@ -231,9 +231,6 @@ function q4Incorrect() {
   answer4El.addEventListener("click", resultsPage);
 }
 
-// - after I have finished answering all questions, a page with "all done!" and my final score tallied up is displayed
-//     - after I am done answering all questions, the timer is stopped, the end time is added to my score, and the score is displayed
-//     - a text prompt for my initials displays on screen, along with a submit button that takes me to the leaderboard page
 function resultsPage() {
   // stop the timer
   clearInterval(timer);
@@ -252,91 +249,67 @@ function resultsPage() {
 
 //   clicking the submit button saves your initials and score to the console, displays the initials with your score in the leaderboard
 if (page.includes("index")) {
-    submitInitialsEl.addEventListener("click", submitScore);
+  submitInitialsEl.addEventListener("click", submitScore);
 }
-
-// localStorage.setItem("highscoresArray", [])
-var highscoresArray = [];
-
-// checking if session exists in local storage:
-// if (localStorage.getItem("session" !== true)) {
-//     // creating session in local storage to hold highscores
-//     highscoresArray.push(JSON.parse(localStorage.getItem('session')));
-//     localStorage.setItem('session', JSON.stringify(highscoresArray));
-// }
 
 function submitScore() {
-    // check to make sure there is something in the initials box
-    if (initials.value.length == 0) {
-        alert("Please Insert Initials in Text Box!")
-        // send error saying "Please put something in the box!"
-    } else {
-        // create object to hold initials and score:
-        localStorage.setItem("initials", JSON.stringify(initialsEl.value))
-        localStorage.setItem("userScore", userScore)
-        var initialsInStorage = localStorage.getItem("initials")
-        console.log(initialsInStorage)
-        console.log(scoresEl)
-        scoresEl.textContent = initialsInStorage
-        
-
-
-        // var initialsAndScore = {
-        //     initials: initialsEl.value,
-        //     score: userScore,
-        // }
-
-        // localStorage.setItem("initialsAndScore", JSON.stringify(initialsAndScore))
-        // scoresEl.textContent = localStorage.getItem("initialsAndScore")
-        
-
-        // // convert the object into an array
-        // highscoresArray = JSON.parse(localStorage.getItem('session')) || [];
-        // // push new data onto array
-        // highscoresArray.push(initialsAndScore)
-        // // put array back into string for local storage
-        // localStorage.setItem('session', JSON.stringify(highscoresArray))
-        // // display initials and score in the highscores page
-        // console.log(localStorage.length)
-
-        // send user to the highscores page
-        // location.href = "./highscores.html";
-
+  // check to make sure there is something in the initials box
+  if (initials.value.length == 0) {
+    alert("Please Insert Initials in Text Box!");
+    // send error saying "Please put something in the box!"
+  } else {
+    var initialsAndScore = {
+        initials: initialsEl.value,
+        score: userScore,
     }
+    // grabbing the array from the Local Storage or creates a new one
+    var highscoreArray = JSON.parse(localStorage.getItem("highscoreArray") || "[]");
+    // pushes the new initialsAndScore value into the highscoreArray that was taken from local storage
+    highscoreArray.push(initialsAndScore);
+    // puts the highscoreArray BACK into localStorage, now with the new value
+    localStorage.setItem("highscoreArray", JSON.stringify(highscoreArray));
+
+    // send user to the highscores page
+    location.href = "./highscores.html";
+  }
 }
 
-// // create variable for submitted scores 
-// var scoresSubmitted = localStorage.getItem("session")
-// console.log(scoresSubmitted)
-// // remove fluff from string
-// var scoresSubmittedClean = scoresSubmitted.split("")
-// // posts score to highscores page
-// scoresEl.textContent = scoresSubmitted
+function highscoresLoad() {
+    clearHighscoresBtnEl= document.getElementById("#clear-highscores-btn")
+    highscoresListEl = document.getElementById("#highscores-list")
+    // grabs the array from Local Storage
+  var highscoreArray = JSON.parse(localStorage.getItem("highscoreArray"));
+  console.log(highscoreArray);
+//   runs through each object in the array and creates a new div for each
+    for (i=0; i < highscoreArray.length; i++) {
+        var li = document.createElement("li",);
+        console.log(li)
+        li.textContent = JSON.stringify(highscoreArray[i]);
+        console.log(li)
+        document.body.appendChild(li)
+    }
+    console.log(clearHighscoresBtnEl)
+}
+// function that clears local storage when pressing the "Clear Highscores" button
+function clearLocalStorage() {
+    localStorage.clear();
+    // refreshes page to show effect
+    location.reload();
 
-
+}
 // if the timer runs out, it sends me to a "times up!" page with my score
 function timeUp() {
-    // stop the timer
-    clearInterval(timer);
-    // update userScore to reflect results
-    scoreEl.textContent = userScore;
-    // show/hide necessary html elements
-    quizTitleEl.textContent = "Time is Up!";
-    quizTitleEl.style.display = "block";
-    questionEl.style.display = "none";
-    answers.style.display = "none";
-    answerResultEl.style.display = "none";
-    scoreIndicatorEL.style.display = "flex";
-    //   show text block for entering initials
-    initialsSectionEl.style.display = "flex";
+  // stop the timer
+  clearInterval(timer);
+  // update userScore to reflect results
+  scoreEl.textContent = userScore;
+  // show/hide necessary html elements
+  quizTitleEl.textContent = "Time is Up!";
+  quizTitleEl.style.display = "block";
+  questionEl.style.display = "none";
+  answers.style.display = "none";
+  answerResultEl.style.display = "none";
+  scoreIndicatorEL.style.display = "flex";
+  //   show text block for entering initials
+  initialsSectionEl.style.display = "flex";
 }
-
-console.log(highscoresArray)
-// - have a leaderboard page that has a stored list of all the times, ordered by highest score.
-//     - store my initials and score to the local memory
-//     - push my initials and score to an unordered list
-//     - order the unordered list from highest to lowest score
-//     - have a button with the option to clear the high score
-//     - have a button withy the option to go back to the first page
-
-// - when i press "Start Quiz" again, I am able to replay the quiz and set a new score that is also saved on the leaderboard along with my previous score, in the correct order.
